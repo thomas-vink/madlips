b3b121a62ba350dafab02aed00712a1e778c0e5d
MSI\thoma <thomasvink11@gmail.com>
2017-02-21 14:27:52 +0100

    html gemaakt

---
+++ madlips.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>madlips</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+		<h1>MadLips</h1>
+	<ul>
+	<li>
+		<a href="">er heerst paniek....</a>
+		<a href="">onkunde</a>
+	</li>
+	</ul>
+
+		<div id="container">
+			<div id="content">
+				<h2>Er heerst paniek....</h2>
+				<p>welk dier zou je nooit als huisdier willen hebben? <input type="input" id="inpt" placeholder="dieren"></p>
+					<br>
+				<p>wie is de belangrijkste persoon in le leven? <input type="input" id="inpt" placeholder="naam"></p>
+					<br>
+				<p>in welk land zou je graag willen wonen? <input type="input" id="inpt" placeholder="landnaam"></p>
+					<br>
+				<p>wat doe je als je je verveeld? <input type="input" id="inpt" placeholder="activiteit"></p>
+					<br>
+				<p>met wel speelgoed speelde je als kin het liefst <input type="input" id="inpt" placeholder="speelgoed"></p>
+					<br>
+				<p>bij welke docent spijbelde je het liefst? <input type="input" id="inpt" placeholder="persoon"></p>
+					<br>
+				<p>als je €100.00,- had wat zou je dan kopen? <input type="input" id="inpt" placeholder="product"></p>
+					<br>
+				<p>wat is je favoriete bezigheid <input type="input" id="inpt" placeholder="activiteit"></p>
+				<input type="submit" id="submit" margin-left:10%;>
+			</div>
+		</div>
+
+	<div id="footer">
+		<p>deze website is gemaakt door: Thomas Vink</p>
+	</div>
+
+</body>
+</html>
---
+++ onkunde.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>madlips</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+		<h1>MadLips</h1>
+	<ul>
+	<li>
+		<a href="">er heerst paniek....</a>
+		<a href="">onkunde</a>
+	</li>
+	</ul>
+
+		<div id="container">
+			<div id="content">
+				<h2>Onkunde</h2>
+				<p>wat zou je graag willen kunnen? <input type="input" id="inpt" placeholder="dieren"></p>
+					<br>
+				<p>met welke persoon kun je goed opschieten?<input type="input" id="inpt" placeholder="naam"></p>
+					<br>
+				<p>wat is je favoriete getal? <input type="input" id="inpt" placeholder="landnaam"></p>
+					<br>
+				<p>wat heb je alrijd bij je als je op vakantie gaat? <input type="input" id="inpt" placeholder="activiteit"></p>
+					<br>
+				<p>wat is je beste persoonlijke eigenshap?<input type="input" id="inpt" placeholder="speelgoed"></p>
+					<br>
+				<p>wat is je slechtste persoonlijke eigenshap? <input type="input" id="inpt" placeholder="persoon"></p>
+					<br>
+				<p>wat is het ergste wat je is overkomen <input type="input" id="inpt" placeholder="product"></p>
+					<br>
+				
+				<input type="submit" id="submit" >
+			</div>
+		</div>
+
+	<div id="footer">
+		<p>deze website is gemaakt door: Thomas Vink</p>
+	</div>
+
+</body>
+</html>
